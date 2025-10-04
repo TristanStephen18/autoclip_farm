ef59df72fea376c715106c0dda59f12331de66f2
TristanStephen18 <trasco_21ur1406@psu.edu.ph>
2025-10-04 13:14:21 +0800

    Added dashboard route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Route, Switch } from "wouter";
 import AuthPage from "./pages/auth/Auth";
 import YouTubeExtractor from "./pages/trials/youtubevideoextractor";
+import { Dashboard } from "./pages/main/Dashboard";
 
 const queryClient = new QueryClient();
 
@@ -11,6 +12,7 @@ export default function App() {
       <Switch>
         <Route path="/" component={AuthPage} />
         <Route path="/yt" component={YouTubeExtractor} />
+        <Route path="/dashboard" component={Dashboard} />
 
         <Route>
           {() => <div className="p-6 text-center">404 – Page not found</div>}

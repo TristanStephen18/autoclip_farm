1c5e865f8685576e6a38c6203490628590ee1f80
TristanStephen18 <trasco_21ur1406@psu.edu.ph>
2025-10-06 22:51:17 +0800

    altered for vite express single port dev

--- server/index.ts
+++ server/index.ts
@@ -3,9 +3,8 @@ import cors from "cors";
 import ytRoute from "./routes/apis/yt.ts";
 import AiClipperRoute from "./routes/apis/gemini.ts";
 import path from "path";
+import ViteExpress from 'vite-express';
 import dotenv from 'dotenv';
-
-
 
 const app = express();
 dotenv.config();
@@ -13,6 +12,8 @@ dotenv.config();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+ViteExpress.config({viteConfigFile: "./client/vite.config.ts"});
 
 //routes
 
@@ -25,11 +26,11 @@ app.use(
   express.static(path.join(process.cwd(), "./server/public/videos"))
 );
 
-app.get("/hello", (req, res) => {
-  res.json({ message: "hello" });
+app.get("/api/hello", (req, res) => {
+  res.send("hello from the server");
 });
 
-app.listen(3000, () => {
+ViteExpress.listen(app, 3000, () => {
   console.log(path.join(process.cwd(), './server/public/videos'));
   console.log("server is listening");
 });
